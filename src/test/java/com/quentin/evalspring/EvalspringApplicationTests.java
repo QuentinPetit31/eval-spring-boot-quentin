45222a102455be22dbdfa8fa7557b6b92f32aef3
QuentinPetit31 <140169105+QuentinPetit31@users.noreply.github.com>
2025-03-19 16:27:39 +0100

    Partie 1

---
+++ src/test/java/com/quentin/evalspring/EvalspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quentin.evalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
